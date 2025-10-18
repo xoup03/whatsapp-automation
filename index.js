af2133dce0cf5f7014796ee9faec0bf9f500b475
xoup03 <xoup.business@gmail.com>
2025-10-18 17:18:56 +0530

    Add CORS support to Express server

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 import express from "express";
 import qrcode from "qrcode";
 import pkg from "whatsapp-web.js";
+import cors from "cors";
 
 const { Client, LocalAuth, MessageMedia, Events } = pkg;
 
 const app = express();
+
+app.use(cors()); 
 app.use(express.json());
 
 let qrCodeData = ""; // store QR temporarily
@@ -111,3 +114,4 @@ const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`🌐 Server started on port ${PORT}`);
 });
+
